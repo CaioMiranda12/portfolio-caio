4370075aaf5d825a5f148a6419e92b7bee5fa861
CaioMiranda12 <caioclm04@gmail.com>
2025-06-20 19:46:18 -0300

    feat: add animation on hero and project sections

--- src/app/_components/hero/index.tsx
+++ src/app/_components/hero/index.tsx
@@ -8,25 +8,40 @@ export function Hero() {
       <div className="container mx-auto px-4 text-center flex flex-col gap-2">
 
         <span
-          className="text-gray-500 text-sm md:text-lg tracking-wide uppercase animate-fade-in"
+          className="text-gray-500 text-sm md:text-lg tracking-wide uppercase"
+          data-aos='fade-in'
         >
           React & Next.js Developer
         </span>
 
         <h2
-          className="text-2xl md:text-4xl text-gray-100 animate-slide-up opacity-0 delay-100"
+          className="text-2xl md:text-4xl text-gray-100"
+          data-aos='fade-up'
+          data-aos-delay="100"
+          data-aos-duration="800"
+          data-aos-easing="ease-out"
         >
           Olá! Me chamo <span className="text-cyan-400 font-bold">Caio Miranda</span>
         </h2>
 
 
-        <p className="text-gray-400 text-xl md:text-3xl animate-slide-up opacity-0 delay-300">
+        <p
+          className="text-gray-400 text-xl md:text-3xl"
+          data-aos='fade-up'
+          data-aos-delay="300"
+          data-aos-duration="800"
+          data-aos-easing="ease-out"
+        >
           Sites modernos com foco em performance e experiência do usuário
         </p>
 
         <Button
-          className="bg-gray-800 hover:bg-gray-700 cursor-pointer w-max mx-auto text-xl p-6 mt-4 animate-slide-up opacity-0"
+          className="bg-gray-800 hover:bg-gray-700 cursor-pointer w-max mx-auto text-xl p-6 mt-4"
           style={{ animationDelay: '500ms' }}
+          data-aos='fade-up'
+          data-aos-delay="500"
+          data-aos-duration="800"
+          data-aos-easing="ease-out"
         >
           Saiba mais
         </Button>
